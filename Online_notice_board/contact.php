fe35e9174ff621794fecd0e5aed23199f1df5f87
Muhammad Sarmad Mujahid <52624788+Sarmad057@users.noreply.github.com>
2019-12-24 14:44:33 +0500

    Online Notice Board

---
+++ Online_notice_board/contact.php
@@ -0,0 +1,50 @@
+<h2>Quick Contact</h2>
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Email</div>
+		<div class="col-sm-5">
+		<input type="email" name="e" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Mobile</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Message</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Send Query" name="save" class="btn btn-success"/>
+		
+		</div>
+	</div>
+		
+		</div>
+		<div class="col-md-4">
+		<h2>Contact us</h2>
+		
+		Name: Sarmad Mujahid<br/>
+		Mobile: 03454402233<br/>
+		Email:sarmadmujahid66@yahoo.com<br/>
+		Website: <a href="https://www.comsats.edu.pk/">CIIT</a>
+		</div>
+	</div>
+	
+	
+</form>	
